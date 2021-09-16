1d8a8388ac62a415c6102a9a5364d9cb48151782
lines_makers <keepinmindsh@gmail.com>
2021-09-16 21:46:00 +0900

    modify project for coding bytebuddy

--- 01_java/bytecode_bytebuddy/src/main/java/lines/bytebuddy/Main.java
+++ 01_java/bytecode_bytebuddy/src/main/java/lines/bytebuddy/Main.java
@@ -1,7 +1,26 @@
 package lines.bytebuddy;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+
+import java.io.File;
+import java.io.IOException;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new TargetClass().getValue());
+
+        // TODO - it does not created with bellow code. need to check tomorrow
+        try {
+            new ByteBuddy().redefine(TargetClass.class)
+                            .method(named("getValue"))
+                                    .intercept(FixedValue.value("Good Night!"))
+                    .make().saveIn(new File("D:/GIT/01_GIT/06_templates_for_all/01_java/bytecode_bytebuddy/build/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println(new TargetClass().getValue());
     }
 }
