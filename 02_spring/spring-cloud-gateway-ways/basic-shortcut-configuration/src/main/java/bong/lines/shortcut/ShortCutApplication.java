f6593c6641c968a25e1a64defe32895703eb17e9
승화정 <keepinmindsh@gmail.com>
2021-09-18 22:16:24 +0900

    add gateway sample

--- 02_spring/spring-cloud-gateway-ways/basic-shortcut-configuration/src/main/java/bong/lines/shortcut/ShortCutApplication.java
+++ 02_spring/spring-cloud-gateway-ways/basic-shortcut-configuration/src/main/java/bong/lines/shortcut/ShortCutApplication.java
@@ -2,10 +2,18 @@ package bong.lines.shortcut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ShortCutApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortCutApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "hello world";
     }
 }
