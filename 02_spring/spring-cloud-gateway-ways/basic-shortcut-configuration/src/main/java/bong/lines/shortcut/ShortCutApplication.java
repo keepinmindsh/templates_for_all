f4488bf165b528b6cbe8140b91703ca51a4cc00d
승화정 <keepinmindsh@gmail.com>
2021-09-18 21:13:36 +0900

    spring cloud gateway - basic configuration

---
+++ 02_spring/spring-cloud-gateway-ways/basic-shortcut-configuration/src/main/java/bong/lines/shortcut/ShortCutApplication.java
@@ -0,0 +1,11 @@
+package bong.lines.shortcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortCutApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortCutApplication.class, args);
+    }
+}
