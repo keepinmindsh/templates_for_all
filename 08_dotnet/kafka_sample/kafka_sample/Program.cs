ecb382addcbdd7384c1564a5a16b5758a9a64889
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2021-10-04 21:32:29 +0900

    kafka for dotnet

---
+++ 08_dotnet/kafka_sample/kafka_sample/Program.cs
@@ -0,0 +1,56 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace kafka_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "consumer.sanha",
+                BootstrapServers = "10.30.0.153:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("test");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            Console.WriteLine($"Consumed Message: '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch(ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+
+            }
+        }
+    }
+}
