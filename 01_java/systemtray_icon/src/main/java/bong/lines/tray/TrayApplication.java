e76eb19b8d7de8aec5c5a15aa2706fca3da3d2cc
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2021-10-11 00:25:13 +0900

    start for tray icon for windows

--- 01_java/systemtray_icon/src/main/java/bong/lines/tray/TrayApplication.java
+++ 01_java/systemtray_icon/src/main/java/bong/lines/tray/TrayApplication.java
@@ -1,5 +1,7 @@
 package bong.lines.tray;
 
+import bong.lines.tray.handler.TrayIconHandler;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,15 +9,27 @@ import java.awt.event.ActionListener;
 public class TrayApplication {
 
     public static void main(String[] args) {
+
+        // src/main/resources/icon 폴더 밑에 computer.png 아이콘 파일을 넘겨주고, 마우스로 올려질 때 보여질 “Example”, 그리고 더블 클릭했을 때 종료되는 코드이다.
         TrayIconHandler.registerTrayIcon(
-                Toolkit.getDefaultToolkit().getImage("src/main/resources/icon/computer.png"),
+                Toolkit.getDefaultToolkit().getImage("src/main/resources/icon/trayicon.png"),
                 "Example",
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        System.exit(0);
+
                     }
                 }
         );
+
+        // 그리고 아래와 같이 addItem() 메소드를 호출하자
+        TrayIconHandler.addItem("Exit", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        TrayIconHandler.displayMessage("Silentsoft", "Benefit the world !", TrayIcon.MessageType.INFO);
     }
 }
