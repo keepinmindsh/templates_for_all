b845a9e89acbe5d029a6eee0a40cddf458742c5c
승화정 <keepinmindsh@gmail.com>
2021-10-17 10:20:52 +0900

    sample project

--- 02_spring/spring-for-starter/src/main/java/bong/lines/sample/SampleApplication.java
+++ 02_spring/spring-for-starter/src/main/java/bong/lines/sample/SampleApplication.java
@@ -5,18 +5,43 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SampleApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SampleApplication.class, args);
+        //SpringApplication.run(SampleApplication.class, args);
+
+
+        Person person = new Person();
+
+        person.sayHello();
+
+        person.name("Bong");
+
+        person.myName();
+
     }
 }
 
 @RestController
 class Controller {
-
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+}
+
+class Person {
+    private String name = "John";
+
+    public void name(String name){
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void myName() {
+        System.out.println("My Name is " + name);
     }
 }
