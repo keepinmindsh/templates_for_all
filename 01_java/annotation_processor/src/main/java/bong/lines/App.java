a9c6959398f12942406681f246b1a33a0bc990ea
Jeong Seung Hwa <keepinmindsh@gmail.com>
2021-12-08 12:50:20 +0900

    custom annotation process for study

---
+++ 01_java/annotation_processor/src/main/java/bong/lines/App.java
@@ -0,0 +1,8 @@
+package bong.lines;
+
+public class App {
+    public static void main(String[] args) {
+
+        Moja moja = new MojaFactory();
+    }
+}
---
+++ 01_java/annotation_processor/src/main/java/bong/lines/Moja.java
@@ -0,0 +1,6 @@
+package bong.lines;
+
+public interface Moja {
+    String pollOut();
+}
+
