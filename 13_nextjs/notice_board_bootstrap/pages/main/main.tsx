5d6e8ce44bd3d16b42a164963f1620d8233ea2e9
Jeong Seung Hwa <keepinmindsh@gmail.com>
2022-02-20 21:22:23 +0900

    added for notice frontend for study

---
+++ 13_nextjs/notice_board_bootstrap/pages/main/main.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Widgets from '../../common/components/elements/widgets'
+import Tasks from "../../common/components/elements/tasks";
+import { useState } from "react"
+import Header from "../../common/components/elements/header";
+
+const Main: NextPage = () => {
+
+    const [ gridData, setGridData ] = useState<[
+        {
+            taskNo : number,
+            custmName : string,
+            title : string,
+            priorityType : string,
+            requireType : string,
+            errorType : string,
+            receiptDate : string,
+            finishedDate : string,
+            stepType : string
+        }
+    ]|null>();
+
+    const applyGridData = ( gridData : [number]|null) : void => {
+        setGridData(gridData)
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Working Progressive</title>
+                <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <div className="card">
+                <div className="card-body">
+                    <Widgets applyFunc={applyGridData} />
+                    <Tasks gridData={gridData} />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Main
