eef84dafc279d673bc6032b5c5282cda408e7350
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2022-04-09 17:51:38 +0900

    java stream

--- 01_java/java_stream/src/main/java/bong/lines/FilesBasic.java
+++ 01_java/java_stream/src/main/java/bong/lines/FilesBasic.java
@@ -1,17 +1,61 @@
 package bong.lines;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FilesBasic {
     public static void main(String[] args) throws Exception {
 //        createFileAndPathsWithFiles();
 
-        writeStringToFileWithFiles();
+//        writeStringToFileWithFiles();
+
+//        readAllStringWithFiles();
+
+//        readAllStringWithFilesLines();
+
+        writeStringToFileWithFilesSecondway();
+    }
+
+    private static void writeStringToFileWithFilesSecondway() throws IOException {
+        String path = "C:\\Temp\\rc5.log";
+
+        Files.writeString(Paths.get(path), "안녕하세요", StandardCharsets.UTF_8);
+    }
+
+    private static void readAllStringWithFilesLines() {
+        String path = "C:\\Temp\\rc5.log";
+
+        try (Stream<String> lines = Files.lines(Paths.get(path), StandardCharsets.UTF_8) ) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            lines.forEach(s -> stringBuilder.append(s).append("\n"));
+            System.out.println("stringBuilder = " + stringBuilder);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static void readAllStringWithFiles() {
+        String path = "C:\\Temp\\rc5.log";
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(path), StandardCharsets.UTF_8)) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String string = null;
+            while ((string = bufferedReader.readLine()) != null) {
+                stringBuilder.append(string).append("\n");
+            }
+            System.out.println("stringBuilder = " + stringBuilder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void writeStringToFileWithFiles() throws IOException {
