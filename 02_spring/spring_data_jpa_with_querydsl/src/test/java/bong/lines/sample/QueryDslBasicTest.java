294f50e2157b7976ccbea62b483453ab58eb0d36
승화정 <keepinmindsh@gmail.com>
2022-05-04 15:09:23 +0900

    KEEP-105 - subquery with querydsl

--- 02_spring/spring_data_jpa_with_querydsl/src/test/java/bong/lines/sample/QueryDslBasicTest.java
+++ 02_spring/spring_data_jpa_with_querydsl/src/test/java/bong/lines/sample/QueryDslBasicTest.java
@@ -6,6 +6,7 @@ import bong.lines.sample.model.entity.QTeam;
 import bong.lines.sample.model.entity.Team;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -23,6 +24,7 @@ import java.util.List;
 import static bong.lines.sample.model.entity.QMember.*;
 import static bong.lines.sample.model.entity.QMember.member;
 import static bong.lines.sample.model.entity.QTeam.team;
+import static com.querydsl.jpa.JPAExpressions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -440,7 +442,79 @@ public class QueryDslBasicTest {
 
         boolean loaded = emf.getPersistenceUnitUtil().isLoaded(memberOne.getTeam());
 
-        assertThat(loaded).as("패치 조인 미적용").isTrue();ß
+        assertThat(loaded).as("패치 조인 미적용").isTrue();
     }
 
+    @Test
+    @DisplayName("Sub Query 작성시 JPA Expression 활용 예제")
+    public void testSubQuery(){
+
+        /***
+         * select member1
+         * from Member member1
+         * where member1.age = (select max(MemberSub.age)
+         * from Member MemberSub)
+         */
+        QMember memberSub = new QMember("MemberSub");
+
+        List<Member> fetch = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.age.eq(
+                                select(memberSub.age.max())
+                                        .from(memberSub)
+                        )
+                ).fetch();
+
+        assertThat(fetch).extracting("age")
+                .containsExactly(40);
+
+        List<Member> fetchAVG = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.age.goe(
+                                select(memberSub.age.avg())
+                                        .from(memberSub)
+                        )
+                ).fetch();
+
+        assertThat(fetchAVG).extracting("age")
+                .containsExactly(40);
+
+        List<Member> fetchIn = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.age.in(
+                                select(memberSub.age)
+                                        .from(memberSub)
+                                        .where(memberSub.age.gt(10))
+                        )
+                ).fetch();
+
+        assertThat(fetchIn).extracting("age")
+                .containsExactly(20.30,40);
+    }
+
+    @Test
+    public void testSelectSubQuery(){
+
+        /**
+         * JPA JPQL 서브 쿼리의 한계점으로 from 절의 서브 쿼리 (인라인 뷰)는 지원하지 않는다.
+         * 딩연히 QueryDSL도 지원하지 않는다. 하이버네이트 구현체를 사용하면 select 절의 서브쿼리를 지원한다.
+         * QueryDSL도 하이버네이트 구현체를 사용하면 select 절의 서브쿼리를 지원한다. s
+         *  - 서브쿼리를 join으로 변경한다.
+         *  - 애플리케이션 쿼리를 2번 분리해서 실행한다.
+         *  - nativeSQL 를 사용한다.
+          */
+        QMember memberSub = new QMember("MemberSub");
+
+        List<Tuple> fetch = jpaQueryFactory
+                .select(member.username,
+                        select(memberSub.age.avg())
+                                .from(memberSub)
+                )
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple: fetch) {
+            System.out.println("tuple = " + tuple) ;
+        }
+    }
 }
