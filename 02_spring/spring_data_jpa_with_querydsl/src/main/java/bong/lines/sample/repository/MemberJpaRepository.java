c5f0d0e69c7c8340c26833ed9603e073248029c1
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2022-05-07 21:16:33 +0900

    KEEP-107 순수 JPA와 QueryDSL

--- 02_spring/spring_data_jpa_with_querydsl/src/main/java/bong/lines/sample/repository/MemberJpaRepository.java
+++ 02_spring/spring_data_jpa_with_querydsl/src/main/java/bong/lines/sample/repository/MemberJpaRepository.java
@@ -12,6 +12,8 @@ import java.util.Optional;
 @Repository
 public class MemberJpaRepository {
 
+    // EntityManager와 JPAQueryFactory의 동시성은 모두 EntityManager에 의해서 동작하는데,
+    // Transaction 단위로 Thread를 분리해서 사용할 수 있게 되어 있다.
     private final EntityManager entityManager;
     private final JPAQueryFactory jpaQueryFactory;
 
@@ -46,6 +48,7 @@ public class MemberJpaRepository {
                 .getResultList();
     }
 
+    // 컴파일 시점에 에러가 나기 때문에 사전 코드 체크가 가능함.
     public List<Member> findByUsername_Querydsl(String username){
         return jpaQueryFactory
                 .selectFrom(QMember.member)
