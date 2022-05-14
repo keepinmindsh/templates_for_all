3034aca246a4151252069b8b64376eddb60f8826
승화정 <keepinmindsh@gmail.com>
2022-05-14 19:28:53 +0900

    apply jpa gradle module

--- 13_gradle/multimodule_with_querydsl/entity_member/src/main/java/bong/lines/entity/Member.java
+++ 13_gradle/multimodule_with_querydsl/entity_member/src/main/java/bong/lines/entity/Member.java
@@ -2,10 +2,12 @@ package bong.lines.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Member {
 
+    @Id
     @Column(name = "MEMBER_ID")
     private Long id;
 }
