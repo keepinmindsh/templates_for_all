62d9a9e5891310de1e5e8146267e8eb96e12a499
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2022-05-17 22:53:45 +0900

    KEEP-119 Spring Boot Basic Setting

---
+++ 02_spring/spring-boot-with-backend/src/main/java/bong/lines/Sample1Application.java
@@ -0,0 +1,7 @@
+package bong.lines;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample1Application {
+}
---
+++ 02_spring/spring-boot-with-backend/src/main/java/bong/lines/Sample2Application.java
@@ -0,0 +1,9 @@
+package bong.lines;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan(basePackageClasses = Sample2Application.class)
+public class Sample2Application {
+}
