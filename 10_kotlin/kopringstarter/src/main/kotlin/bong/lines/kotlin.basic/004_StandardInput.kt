016cb5fbe8cbf57fadfd331e6d8c6f6a54f69030
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2022-06-26 02:48:15 +0900

    Standard Input - Sample

---
+++ 10_kotlin/kopringstarter/src/main/kotlin/bong/lines/kotlin.basic/004_StandardInput.kt
@@ -0,0 +1,47 @@
+package bong.lines.kotlin.basic
+
+import java.io.File
+import java.io.InputStream
+import java.util.*
+
+fun main() {
+    standard_readLine()
+
+    standard_readLine_toInt()
+
+    standard_readFile()
+
+    standard_multiReadLine()
+}
+
+private fun standard_readLine_toInt() {
+    val numberInput = readLine()!!.toInt()
+    println("입력 받은 내용(Int): $numberInput")
+}
+
+private fun standard_readLine() {
+    val input = readLine()
+    println("입력 받은 내용: $input")
+}
+
+private fun standard_multiReadLine(){
+    val s = Scanner(System.`in`)
+    val a = s.nextInt()
+    val n = s.nextInt()
+    s.nextLine()
+    val line = s.nextLine()
+    val arr = line.split(" ")
+
+    for(it in arr){
+        if(it.toInt() < n)
+            print("$it ")
+    }
+}
+
+private fun standard_readFile(){
+    val path = "/Users/lines/sources/01_bonggit/templates_for_all/10_kotlin/kopringstarter/docs/kotlin/compare/README.md"
+    val file = File(path)
+    val inputStream: InputStream = file.inputStream()
+    val text = inputStream.bufferedReader().use { it.readText() }
+    println(text)
+}
