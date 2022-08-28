6d56471f9540753d6b49a88765e5f78c4cab742a
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2022-08-28 11:15:27 +0900

    golang for study

--- 03_go/GinSample/main.go
+++ 03_go/GinSample/main.go
@@ -1,12 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"GinSample/util"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
+
 	router.GET("/ping", func(context *gin.Context) {
 		context.String(200, "pong")
 	})
+
+	router.GET("/albums", util.GetAlbums)
+
+	router.GET("/albums/:id", util.GetAlbumByID)
+
+	router.POST("/albums", util.PostAlbums)
+
+	router.GET("/:name/:id", util.BindUrl)
+
+	router.GET("/getb", util.GetDataB)
+
+	router.GET("/getc", util.GetDataC)
+
+	router.GET("/getd", util.GetDataD)
+
+	router.GET("/testing", util.StartPage)
+
+	router.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "<br>",
+		}
+
+		// will output : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
+		c.AsciiJSON(http.StatusOK, data)
+	})
+
 	return router
 }
 
---
+++ 03_go/GinSample/util/bindurl.go
@@ -0,0 +1,17 @@
+package util
+
+import "github.com/gin-gonic/gin"
+
+type PersonBind struct {
+	ID   string `uri:"id" binding:"required,uuid"`
+	Name string `uri:"name" binding:"required"`
+}
+
+func BindUrl(c *gin.Context) {
+	var person PersonBind
+	if err := c.ShouldBindUri(&person); err != nil {
+		c.JSON(400, gin.H{"msg": err})
+		return
+	}
+	c.JSON(200, gin.H{"name": person.Name, "uuid": person.ID})
+}
