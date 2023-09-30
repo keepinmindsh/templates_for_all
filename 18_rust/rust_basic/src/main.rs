f9ad380b8015728e19db870e2022dc98c758fb82
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-09-30 15:28:05 +0900

    added sample for rust study code

--- 18_rust/rust_basic/src/main.rs
+++ 18_rust/rust_basic/src/main.rs
@@ -1,6 +1,11 @@
 mod network;
 use network::server::connect;
 
+mod basic;
+use basic::primitives::primitives::sample_primitives;
+
 fn main() {
-    connect()
+    connect();
+
+    sample_primitives();
 }
